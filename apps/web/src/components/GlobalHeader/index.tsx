319937705739c4149cfdcfa8c9a2703c483f6bf0
Katsushi OUGI <ougi@to-r.net>
2025-09-17 13:56:04 +0900

    feat: add react-avatar dependency and integrate Avatar component in GlobalHeader

--- apps/web/src/components/GlobalHeader/index.tsx
+++ apps/web/src/components/GlobalHeader/index.tsx
@@ -1,11 +1,25 @@
+import Avatar from "react-avatar";
+
+import { useAuth } from "@/hooks";
 import { APP_NAME } from "@packages/config";
 import { HamburgerMenu } from "./HamburgerMenu";
 import styles from "./styles.module.css";
 
 export const GlobalHeader = () => {
+  const { user } = useAuth();
+
   return (
     <header className={styles.root}>
-      <div />
+      <div>
+        {user && (
+          <Avatar
+            src={user.photoURL ?? undefined}
+            name={user.displayName ?? undefined}
+            size="40"
+            round={true}
+          />
+        )}
+      </div>
       <h1 className={styles.title}>{APP_NAME}</h1>
       <HamburgerMenu />
     </header>
