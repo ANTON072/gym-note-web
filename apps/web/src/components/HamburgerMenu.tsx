3af62124621ebbf948da38fcd3a43a50b2849794
Katsushi OUGI <ougi@to-r.net>
2025-09-19 11:25:23 +0900

    fix: correct path IDs in HamburgerMenu SVG for consistency

--- apps/web/src/components/HamburgerMenu.tsx
+++ apps/web/src/components/HamburgerMenu.tsx
@@ -5,9 +5,9 @@ export const HamburgerMenu = (props: React.ButtonHTMLAttributes<HTMLButtonElemen
     <button type="button" aria-label="Open menu" className={`${styles.button} tap`} {...props}>
       <svg viewBox="0 0 40 40" className={styles.icon}>
         <title>Open menu</title>
-        <path id="top-line-1" d="M8,12 L32,12 Z" />
-        <path id="middle-line-1" d="M8,20 L32,20 Z" />
-        <path id="bottom-line-1" d="M8,28 L32,28 Z" />
+        <path id="top-line" d="M8,12 L32,12 Z" />
+        <path id="middle-line" d="M8,20 L32,20 Z" />
+        <path id="bottom-line" d="M8,28 L32,28 Z" />
       </svg>
     </button>
   );
