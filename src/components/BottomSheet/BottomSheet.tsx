a2b600c631357349c3056e22856e5881eaf0f3fb
Katsushi OUGI <ougi@to-r.net>
2025-10-02 13:40:21 +0900

    feat: enhance BottomSheet component with backdrop tap handling and update DashboardPage integration

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -3,18 +3,34 @@ import { Sheet, type SheetProps, type SheetRef } from "react-modal-sheet";
 
 interface Props extends Omit<SheetProps, "children"> {
   children: React.ReactNode;
+  closeOnBackdropTap?: boolean;
+  onBackdropTap?: () => void;
 }
 
-export const BottomSheet = React.forwardRef<SheetRef, Props>(({ children, ...sheetProps }, ref) => {
-  return (
-    <Sheet ref={ref} {...sheetProps}>
-      <Sheet.Container>
-        <Sheet.Header />
-        <Sheet.Content>{children}</Sheet.Content>
-      </Sheet.Container>
-      <Sheet.Backdrop />
-    </Sheet>
-  );
-});
+export const BottomSheet = React.forwardRef<SheetRef, Props>(
+  ({ children, closeOnBackdropTap, onBackdropTap, onClose, ...sheetProps }, ref) => {
+    // onBackdropTapが指定されている場合はcloseOnBackdropTapのデフォルトをfalseにする
+    const shouldCloseOnTap = closeOnBackdropTap ?? !onBackdropTap;
+
+    const handleBackdropTap = () => {
+      if (onBackdropTap) {
+        onBackdropTap();
+      }
+      if (shouldCloseOnTap && onClose) {
+        onClose();
+      }
+    };
+
+    return (
+      <Sheet ref={ref} onClose={onClose} {...sheetProps}>
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content>{children}</Sheet.Content>
+        </Sheet.Container>
+        <Sheet.Backdrop onTap={handleBackdropTap} />
+      </Sheet>
+    );
+  },
+);
 
 BottomSheet.displayName = "BottomSheet";
--- src/features/dashboard/components/DashboardPage.tsx
+++ src/features/dashboard/components/DashboardPage.tsx
@@ -4,15 +4,20 @@ import styles from "./DashboardPage.module.css";
 export function DashboardPage() {
   const { onOpen, onClose, ref, isOpen } = useBottomSheet();
 
-  console.log("isOpen", isOpen);
-
   return (
     <div>
       <h1>Home</h1>
       <button type="button" onClick={onOpen} className={styles["trigger-button"]}>
         Open Bottom Sheet
       </button>
-      <BottomSheet ref={ref} isOpen={isOpen} onClose={onClose}>
+      <BottomSheet
+        ref={ref}
+        isOpen={isOpen}
+        onClose={onClose}
+        disableDismiss
+        disableDrag
+        closeOnBackdropTap={false}
+      >
         <p>Hello World</p>
       </BottomSheet>
     </div>
