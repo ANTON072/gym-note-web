7d21b86e7320f031249624e95027dcb7390e0ecf
Katsushi OUGI <ougi@to-r.net>
2025-10-07 11:50:39 +0900

    feat: update NoteStatusChip component with improved styling and icon display

--- src/features/notes/components/NoteStatusChip.tsx
+++ src/features/notes/components/NoteStatusChip.tsx
@@ -1,3 +1,7 @@
+import { GoIssueOpened } from "react-icons/go";
+import { GoIssueDraft } from "react-icons/go";
+import { GoIssueReopened } from "react-icons/go";
+
 import { NOTE_STATUS, type NoteStatus } from "../constants/noteStatus";
 import styles from "./NoteStatusChip.module.css";
 
@@ -8,5 +12,23 @@ interface Props {
 export const NoteStatusChip = ({ status }: Props) => {
   const statusLabel = NOTE_STATUS.find((s) => s.value === status)?.label;
 
-  return <div className={styles.NoteStatusChip}>{statusLabel}</div>;
+  const showIcon = () => {
+    switch (status) {
+      case "active":
+        return <GoIssueOpened />;
+      case "completed":
+        return <GoIssueReopened />;
+      case "archived":
+        return <GoIssueDraft />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className={`${styles.NoteStatusChip} ${styles[status]}`}>
+      {showIcon()}
+      {statusLabel}
+    </div>
+  );
 };
