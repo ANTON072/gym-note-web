235f93e66a61378fdfb3757c6b2c2ccfa5729985
Katsushi OUGI <ougi@to-r.net>
2025-10-07 11:53:12 +0900

    fix: standardize quotation marks in mypage route file for consistency

--- src/routes/mypage.tsx
+++ src/routes/mypage.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/mypage')({
+export const Route = createFileRoute("/mypage")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/mypage"!</div>
+  return <div>Hello "/mypage"!</div>;
 }
