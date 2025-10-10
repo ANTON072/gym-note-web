d441e1931f87ef04b4290e2c0cc758cdf8c4738f
Katsushi OUGI <ougi@to-r.net>
2025-10-10 16:24:02 +0900

    feat: 種目一覧ページにフィルタリング機能を追加し、選択肢を更新しました

--- src/features/exercise/components/ExerciseListPage.tsx
+++ src/features/exercise/components/ExerciseListPage.tsx
@@ -5,6 +5,7 @@ import Skeleton from "react-loading-skeleton";
 import { BODY_PART_OPTIONS } from "@/constants/bodyParts";
 import { useIsMutating } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
+import { useState } from "react";
 import { useGetExercises } from "../hooks/useExerciseApi";
 import type { Exercise } from "../schema";
 import { DeleteExerciseButton } from "./DeleteExerciseButton";
@@ -15,37 +16,53 @@ export function ExerciseListPage() {
   const exercises: Exercise[] = Array.isArray(data) ? data : [];
   const isMutating = useIsMutating() > 0;
 
+  const [filteredBodyPart, setFilteredBodyPart] = useState("");
+
+  const filteredExercises = exercises.filter((exercise) => {
+    if (filteredBodyPart === "") {
+      return true;
+    }
+    return exercise.body_part === filteredBodyPart;
+  });
+
   return (
     <>
       <PageTitle title="種目一覧" />
       <div className={styles.wrapper}>
         <div className={styles.indexForm}>
-          <form>
-            <InputField
-              label="種目名で絞り込み"
-              style={{
-                position: "relative",
-                top: " calc(-1 * var(--form-font-size))",
+          <InputField
+            label="種目名で絞り込み"
+            style={{
+              position: "relative",
+              top: " calc(-1 * var(--form-font-size))",
+            }}
+          >
+            <Select
+              name="body_part"
+              disabled={isMutating}
+              onChange={(e) => {
+                const target = e.target.value;
+                setFilteredBodyPart(target);
               }}
+              value={filteredBodyPart}
             >
-              <Select name="body_part" disabled={isMutating}>
-                {Array.isArray(BODY_PART_OPTIONS)
-                  ? BODY_PART_OPTIONS.map(({ value, label }) => (
-                      <option key={value} value={value}>
-                        {label}
-                      </option>
-                    ))
-                  : null}
-                <option value="">すべて</option>
-              </Select>
-            </InputField>
-          </form>
+              {Array.isArray(BODY_PART_OPTIONS)
+                ? BODY_PART_OPTIONS.map(({ value, label }) => (
+                    <option key={value} value={value}>
+                      {label}
+                    </option>
+                  ))
+                : null}
+              <option value="">すべて</option>
+            </Select>
+          </InputField>
+
           <Button to="/exercises/new">新規登録</Button>
         </div>
         {isLoading && !isFetched && <Skeleton count={5} height={40} style={{ marginBottom: 10 }} />}
         {isFetched && (
           <Table
-            data={exercises}
+            data={filteredExercises}
             columns={[
               {
                 key: "name",
