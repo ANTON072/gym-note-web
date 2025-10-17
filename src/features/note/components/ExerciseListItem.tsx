baacd94f28b554843b6408987093f5b4dd8d721a
Katsushi OUGI <ougi@to-r.net>
2025-10-18 00:18:50 +0900

    feat: ExerciseListItemのスタイルを削除し、UIコンポーネントを使用して再構築しました

--- src/features/note/components/ExerciseListItem.tsx
+++ src/features/note/components/ExerciseListItem.tsx
@@ -1,6 +1,6 @@
-import { GoPencil } from "react-icons/go";
-import { GoPlusCircle } from "react-icons/go";
-import styles from "./ExerciseListItem.module.css";
+import { Button } from "@/components/ui/button";
+import { Card, CardAction, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { GoPencil, GoPlusCircle } from "react-icons/go";
 
 export const ExerciseListItem = () => {
   // 仮のデータ（後でpropsから受け取るように変更）
@@ -11,35 +11,35 @@ export const ExerciseListItem = () => {
   ];
 
   return (
-    <div className={styles.ExerciseListItem}>
-      <div className={styles.ExerciseListItem__head}>
-        <div className={styles.ExerciseListItem__name}>1. ベンチプレス</div>
-        <button type="button" className={styles.ExerciseListItem__editButton}>
-          <span className={styles.ExerciseListItem__editButtonInner}>
-            <GoPencil />
-          </span>
-        </button>
-      </div>
-      <div className={styles.ExerciseListItem__body}>
-        <div className={styles.ExerciseListItem__sets}>
+    <Card className="!border-stone-300 gap-1 pb-2">
+      <CardHeader className="px-3">
+        <CardTitle className="text-lg font-bold">1. ベンチプレス</CardTitle>
+      </CardHeader>
+      <CardContent className="px-3">
+        <div className="grid">
           {sets.map((set, index) => (
-            <div key={set.id} className={styles.ExerciseListItem__set}>
-              <div className={styles.ExerciseListItem__setNumber}>{index + 1}セット</div>
-              <div className={styles.ExerciseListItem__setDetail}>
+            <div
+              key={set.id}
+              className="grid grid-cols-[minmax(3rem,auto)_1fr] py-2 gap-2 border-b border-stone-200 last:border-b-0"
+            >
+              <div className="text-stone-700 font-semibold dark:text-stone-400">
+                {index + 1}セット
+              </div>
+              <div className="flex items-center gap-2">
                 <div>{set.weight}kg</div>
                 <div>×</div>
                 <div>{set.reps}回</div>
               </div>
             </div>
           ))}
-          <div className={styles.ExerciseListItem__addSet}>
-            <button className={styles.ExerciseListItem__addSetButton} type="button">
+          <div className="flex justify-end items-center">
+            <Button variant="ghost" className="gap-2 font-bold">
               セットの追加
               <GoPlusCircle />
-            </button>
+            </Button>
           </div>
         </div>
-      </div>
-    </div>
+      </CardContent>
+    </Card>
   );
 };
