6b5132daf814f7b109dfd133277ecb1e742837e4
Katsushi OUGI <ougi@to-r.net>
2025-10-25 23:47:07 +0900

    feat: MutateButtonコンポーネントのインポートパスを修正

--- src/components/MutateButton.tsx
+++ src/components/MutateButton.tsx
@@ -1,7 +1,7 @@
 import { useIsMutating } from "@tanstack/react-query";
 import type { ComponentProps } from "react";
-import { Button } from "./ui/button";
-import { Spinner } from "./ui/spinner";
+import { Button } from "./shadcn/button";
+import { Spinner } from "./shadcn/spinner";
 
 export const MutateButton = ({ children, disabled, ...props }: ComponentProps<typeof Button>) => {
   const isMutating = useIsMutating() > 0;
